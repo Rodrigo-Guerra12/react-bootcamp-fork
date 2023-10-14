7b8c5ccb1416707f7279ba6576fdad1f1d8cce3c
Totenkopf25 <pablogatti25@gmail.com>
2023-10-14 13:08:18 -0300

    agregando tareas en backend

--- src/Components/AddTaskForm/ButtonAgregar.jsx
+++ src/Components/AddTaskForm/ButtonAgregar.jsx
@@ -1,10 +1,20 @@
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
-export default function ButtonAgregar() {
+// eslint-disable-next-line react/prop-types
+export default function ButtonAgregar({ onAddHandler }) {
+  const onClickHandler = () => {
+    const newTask = {
+      description: "bla bla",
+      isCompleted: true,
+    };
+    onAddHandler(newTask);
+  };
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="contained">Agregar</Button>
+      <Button onClick={onClickHandler} variant="contained">
+        Agregar
+      </Button>
     </Stack>
   );
 }
--- src/Components/AddTaskForm/CardForm.jsx
+++ src/Components/AddTaskForm/CardForm.jsx
@@ -1,28 +1,49 @@
 // import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import BasicTextFields from './InputForm';
-import ButtonAgregar from './ButtonAgregar';
-
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import BasicTextFields from "./InputForm";
+import ButtonAgregar from "./ButtonAgregar";
 
 export default function BasicCard() {
+  const url = "http://localhost:3000/tasks";
+  const headers = new Headers({ "Content-Type": "application/json" });
+  const onAddHandler = async (newTask) => {
+    try {
+      console.log("Task", newTask);
+      const response = await fetch(url, {
+        body: JSON.stringify(newTask),
+
+        headers,
+        method: "POST",
+      });
+
+      const jsonData = await response.json();
+      console.log("Jsondata", jsonData);
+      // setTasks(jsonData);
+
+      // setFilteredTaskList(jsonData);
+    } catch (error) {
+      console.log("Error al traer datos:", error);
+    }
+  };
+
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-      <Typography variant="h5" component="div">
+        <Typography variant="h5" component="div">
           Nueva Tarea
         </Typography>
         <Typography sx={{ mb: 2 }} color="text.secondary">
           Descripcion
         </Typography>
 
-        <BasicTextFields/>
+        <BasicTextFields />
       </CardContent>
-      
+
       <CardActions>
-        <ButtonAgregar/>
+        <ButtonAgregar onAddHandler={onAddHandler} />
       </CardActions>
     </Card>
   );
